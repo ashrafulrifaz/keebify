8aad198165654a90b60f0acbf97d2655214b9767
ashrafulrifaz <ashrafulislamrifaz@gmail.com>
2026-08-31 16:34:17 +0600

    added admin access to dashboard

--- src/proxy.js
+++ src/proxy.js
@@ -4,8 +4,9 @@ import { NextResponse } from 'next/server';
 export default withAuth(
   function proxy(req) {
     const token = req.nextauth.token
-    console.log('TOKEN:', token);
     const path = req.nextUrl.pathname
+
+    console.log('TOKEN:', token);
 
     if(path.startsWith('/dashboard') && token?.role !== 'admin') {
       return NextResponse.redirect(new URL('/', req.url))
